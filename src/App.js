3ba47ee221605792676645e178acdc10d5b810ed
michaltruszczynski <michal.truszczynski2@gmail.com>
2018-09-29 23:52:36 +0200

    css update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -56,16 +56,10 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    };
 
     let persons = null;
+    let btnClass = '';
+
 
     if (this.state.showPerson) {
       persons = (
@@ -93,27 +87,27 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.Red;
 
     }
 
-    let classes = [];
+    let assignedClasses = [];
 
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
 
     return (
 
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm ract app</h1>
-          <p className={classes.join(' ')}>This is really working</p>
+          <p className={assignedClasses.join(' ')}>This is really working</p>
           <button
-            style={style}
+          className={btnClass}
             onClick={this.togglePersonHandler}>Switch Name</button>
           {persons}
         </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classes from  './Person.css';
 
 
 import './Person.css'
@@ -7,7 +8,7 @@ import './Person.css'
 const person = (props) => {
 
     return (
-        <div className="Person">
+        <div className={classes.Person}>
             <p onClick={props.click}> I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
